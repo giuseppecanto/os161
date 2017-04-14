3b5757363223ada292daaac82ae160d7974e6204
giuseppecanto <giuseppecanto1993@gmail.com>
2017-04-14 14:29:08 +0200

    os161-base-2.0.2

---
+++ compile/LAB/.depend.lab.c
@@ -0,0 +1,6 @@
+lab.o: ../../main/lab.c ../../include/types.h ../../include/kern/types.h \
+ includelinks/kern/machine/types.h includelinks/machine/types.h \
+ ../../include/kern/errno.h ../../include/kern/reboot.h \
+ ../../include/kern/unistd.h ../../include/limits.h \
+ ../../include/kern/limits.h ../../include/lib.h ../../include/cdefs.h \
+ opt-noasserts.h ../../include/test.h opt-sfs.h opt-net.h opt-lab.h
---
+++ compile/LAB/opt-lab.h
@@ -0,0 +1,5 @@
+/* Automatically generated; do not edit */
+#ifndef _OPT_LAB_H_
+#define _OPT_LAB_H_
+#define OPT_LAB 1
+#endif /* _OPT_LAB_H_ */
